1ae9ada9f7a3f9ff01ddca255cd3e8400874c827
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2025-08-13 00:50:46 +0300

    impl tabs

---
+++ app/(tabs)/tasks.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TaskList() {
+  return (
+    <View>
+      <Text>TaskList</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
