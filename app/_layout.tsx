98fc6c89367b71638b5a6ec277e815fdbb474d57
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2025-08-12 16:26:25 +0300

    project reset to the default

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,5 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { Text, View } from "react-native";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <View>
-        <Text>hello</Text>
-      </View>
-    </ThemeProvider>
-  );
+  return <Stack />;
 }
